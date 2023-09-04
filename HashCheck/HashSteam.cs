cb029c67aa2bea5fe4de2c15aab59559d9872778
Vladislav <vlad1988.1@gmail.com>
2023-09-04 18:19:01 +0300

    Добавьте файлы проекта.

---
+++ HashCheck/HashSteam.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+
+namespace HashCheck
+{
+    internal class HashSteam
+    {
+        private readonly string FileLocation;
+        public HashSteam(string fileLocation)
+        {
+            FileLocation = fileLocation;
+        }
+
+        public string HashMD5()
+        {
+            using (FileStream fStream = File.OpenRead(FileLocation))
+            {
+                return GetHash<MD5>(fStream);
+            };
+        }
+
+        public string HashSHA1()
+        {
+            using (FileStream fStream = File.OpenRead(FileLocation))
+            {
+                return GetHash<SHA1>(fStream);
+            };
+        }
+
+        public string HashSHA256()
+        {
+            using (FileStream fStream = File.OpenRead(FileLocation))
+            {                
+                return GetHash<SHA256>(fStream);
+            };
+        }
+        public string HashSHA384()
+        {
+            using (FileStream fStream = File.OpenRead(FileLocation))
+            {
+                return GetHash<SHA384>(fStream);
+            };
+        }
+
+        public string HashSHA512()
+        {
+            using (FileStream fStream = File.OpenRead(FileLocation))
+            {
+                return GetHash<SHA512>(fStream);
+            };
+        }
+                
+        private string GetHash<T>(Stream stream) where T : HashAlgorithm
+        {
+            MethodInfo? create = typeof(T).GetMethod("Create", Array.Empty<Type>());
+            if (create != null)
+            {
+                using T? crypt = create.Invoke(null, null) as T;
+                byte[] hashBytes = crypt!.ComputeHash(stream);
+                return BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLowerInvariant();
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ HashCheck/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace HashCheck
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var hashSteam = new HashSteam(args[0]);
+                switch (args[1])
+                {
+                    case "SHA512":
+                        Console.WriteLine("Подсчет хеша SHA512:");
+                        Console.WriteLine(hashSteam.HashSHA512());
+                        break;
+
+                    case "SHA384":
+                        Console.WriteLine("Подсчет хеша SHA384:");
+                        Console.WriteLine(hashSteam.HashSHA384());
+                        break;
+
+                    case "SHA256":
+                        Console.WriteLine("Подсчет хеша SHA256:");
+                        Console.WriteLine(hashSteam.HashSHA256());
+                        break;
+
+                    case "SHA1":                       
+                        Console.WriteLine("Подсчет хеша SHA1:");
+                        Console.WriteLine(hashSteam.HashSHA1());
+                        
+                        break;
+
+                    case "MD5":                     
+                        Console.WriteLine("Подсчет хеша MD5:");
+                        Console.WriteLine(hashSteam.HashMD5());
+                        break;
+
+                    case "All":
+
+                        Console.WriteLine("Подсчет хеша SHA512:");
+                        Console.WriteLine(hashSteam.HashSHA512());
+
+                        //Console.WriteLine("Подсчет хеша SHA384:");
+                        //Console.WriteLine(hashSteam.HashSHA384());
+
+                        Console.WriteLine("Подсчет хеша SHA256:");
+                        Console.WriteLine(hashSteam.HashSHA256());
+
+                        Console.WriteLine("Подсчет хеша SHA1:");
+                        Console.WriteLine(hashSteam.HashSHA1());
+
+                        Console.WriteLine("Подсчет хеша MD5:");
+                        Console.WriteLine(hashSteam.HashMD5());
+
+                        break;
+                }      
+            }
+            if (Console.ReadLine() == "author")
+            {
+                Console.WriteLine("Автор: Чепурин Владислав");
+                Console.ReadKey();
+            }
+        }      
+    }
+}
